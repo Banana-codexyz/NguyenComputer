35f70312d7d547310d73ec3e9fb57f0ede61dc18
Nguyen <nguyenkhoinguyen139@gmail.com>
2024-03-13 20:37:39 +0700

    first commit

---
+++ src/main/java/com/nguyenz/nguyencomputer/NguyenComputerApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenz.nguyencomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenComputerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenComputerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nguyenz/nguyencomputer/NguyenComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenz.nguyencomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenComputerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
