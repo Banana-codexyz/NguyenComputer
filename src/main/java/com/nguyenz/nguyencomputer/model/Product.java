d713035db290a3d764d46ec40f89a9adb0d62259
ndvn-Nguyen <nguyen11.nguyen@nttdata.com>
2024-03-14 17:00:58 +0700

    gen id

--- src/main/java/com/nguyenz/nguyencomputer/model/Product.java
+++ src/main/java/com/nguyenz/nguyencomputer/model/Product.java
@@ -1,9 +1,6 @@
 package com.nguyenz.nguyencomputer.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -11,6 +8,7 @@ import lombok.Data;
 @Data
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
